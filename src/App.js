3550c4fedd66096198667f40faacec2cbf7f9a07
Amit Kumar <15000amitkumar@gmail.com>
2023-11-02 18:36:19 +0530

    New

--- src/App.js
+++ src/App.js
@@ -1,20 +1,9 @@
 import React, { useState } from "react";
-import "./App.css";
-import "./components/Item";
-import Item from "./components/Item";
-import Forms from "./components/Form";
 function App() {
-  const [title, setTitle] = useState("Akela Amit");
-
   return (
     <div>
-      <Item name="Amit"></Item>
-      <Item name="Sumit"></Item>
-      <Item name="Varsha"></Item>
-
-      <Forms></Forms>
+      <h1>Amit</h1>
     </div>
   );
 }
-
 export default App;
