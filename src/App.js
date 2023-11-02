4f9deb095edd24a4e79943b061bf5cdbf5e863fb
Amit Kumar <15000amitkumar@gmail.com>
2023-11-02 19:31:44 +0530

    Passed value shown on the ui

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import ChildComponent from "./components/ChildComponent";
 
 function App() {
+  // var t = 100;
+  //t is a regular variable, not a state variable
+
+  const [t, setT] = useState("");
   function handleDataFromChild(data) {
     console.log("Data received from child", data);
+    setT(data);
   }
 
   return (
     <div>
       <h2>Parent Component</h2>
       <ChildComponent onP={handleDataFromChild} />
+      <h1>{t}</h1>
     </div>
   );
 }
--- src/components/ChildComponent.jsx
+++ src/components/ChildComponent.jsx
@@ -1,12 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 
 function ChildComponent(props) {
+  const [new_val, setValue] = useState("");
+
+  function handleInputChange(e) {
+    setValue(e.target.value);
+  }
   function handleClick(e) {
-    props.onP(10);
+    // props.onP(10);
+
+    props.onP(new_val);
   }
   return (
     <div>
       <h3>Child Component</h3>
+      <input
+        type="text"
+        placeholder="Enter Value to Be Passed to Parent"
+        value={new_val}
+        onChange={handleInputChange}
+      />
       <button onClick={handleClick} />
     </div>
   );
