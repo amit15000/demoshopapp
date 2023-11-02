e46c5a1a0005db2c6c8ecd3874ed59304b0a42f7
Amit Kumar <15000amitkumar@gmail.com>
2023-11-02 18:12:40 +0530

    multiple useState

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
+import React, { useState } from "react";
 import "./App.css";
 import "./components/Item";
 import Item from "./components/Item";
+import Forms from "./components/Form";
 function App() {
+  const [title, setTitle] = useState("Akela Amit");
+
   return (
     <div>
       <Item name="Amit"></Item>
       <Item name="Sumit"></Item>
       <Item name="Varsha"></Item>
+
+      <Forms></Forms>
     </div>
   );
 }
