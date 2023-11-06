c725123c3cf70e1fe84f9578a920433eb52bf5f6
Amit Kumar <15000amitkumar@gmail.com>
2023-11-06 19:01:36 +0530

    child to p practice

--- src/App.js
+++ src/App.js
@@ -1,21 +1,26 @@
 import React, { useState } from "react";
 import ChildComponent from "./components/ChildComponent";
+import Child2 from "./components/Child2";
 
 function App() {
   // var t = 100;
   //t is a regular variable, not a state variable
 
-  const [t, setT] = useState("");
-  function handleDataFromChild(data) {
-    console.log("Data received from child", data);
-    setT(data);
+  // const [t, setT] = useState("");
+  // // function handleDataFromChild(data) {
+  // //   console.log("Data received from child", data);
+  // //   setT(data);
+  // // }
+  function handleDataFromChild2(data) {
+    console.log(data);
   }
 
   return (
     <div>
       <h2>Parent Component</h2>
-      <ChildComponent onP={handleDataFromChild} />
-      <h1>{t}</h1>
+      {/* <ChildComponent onP={handleDataFromChild} /> */}
+      <Child2 onQ={handleDataFromChild2} />
+      {/* <h1>{t}</h1> */}
     </div>
   );
 }
---
+++ src/components/Child2.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+function Child2(props) {
+  props.onQ("Amit");
+
+  return <div>Child2</div>;
+}
+
+export default Child2;
