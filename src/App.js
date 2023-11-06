6c0b82f538c97b41b83fd135579f2db94be88bb9
Amit Kumar <15000amitkumar@gmail.com>
2023-11-06 22:30:15 +0530

    All variants of  useEffect

--- src/App.js
+++ src/App.js
@@ -6,8 +6,28 @@ function App() {
   const [data, setData] = useState("");
   console.log(i++);
 
+  // // every time then component renders
+  // useEffect(() => {
+  //   console.log(`Render done- ${i++}`);
+  // });
+
+  // [] dependency list--->mein jab change hoga tab tab chalehga
+  //passed nothing means --- bas ek bar chalega after first rendering of the component.
+
+  // //first
+  // useEffect(() => {
+  //   console.log(`Render done- ${i++}`);
+  // }, []);
+
+  //first + dependency change
+  // useEffect(() => {
+  //   console.log(`Render done- ${i++}`);
+  // }, [data]);
+
+  //add or remove event Listener
   useEffect(() => {
-    console.log(`Render done- ${i++}`);
+    console.log(10);
+    return console.log(12);
   });
 
   console.log(i++);
@@ -18,7 +38,9 @@ function App() {
   };
   return (
     <div className="App">
-      <input type="text" onChange={changeHandler}></input>
+      <input type="text" name="in1" onChange={changeHandler}></input>
+      <br></br>
+      <input type="text" name="in2"></input>
       {/* <input type="text" onChange={(e) => console.log(e.target.value)}></input> */}
     </div>
   );
