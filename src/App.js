5b7c28b9b5772252c6b6c1bef79992125f830165
Amit Kumar <15000amitkumar@gmail.com>
2023-11-06 21:17:51 +0530

    useEffect basic

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
+import { useEffect, useState } from "react";
 import "./App.css";
 import "./components/Item";
-import Item from "./components/Item";
+let i = 1;
 function App() {
+  const [data, setData] = useState("");
+  console.log(i++);
+
+  useEffect(() => {
+    console.log(`Render done- ${i++}`);
+  });
+
+  console.log(i++);
+
+  const changeHandler = (e) => {
+    setData(e.target.value);
+    console.log(data);
+  };
   return (
-    <div>
-      <Item name="Amit"></Item>
-      <Item name="Sumit"></Item>
-      <Item name="Varsha"></Item>
+    <div className="App">
+      <input type="text" onChange={changeHandler}></input>
+      {/* <input type="text" onChange={(e) => console.log(e.target.value)}></input> */}
     </div>
   );
 }
