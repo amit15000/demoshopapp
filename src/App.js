68634927330c75f886f4290ef4149fcb52161020
Amit Kumar <15000amitkumar@gmail.com>
2023-11-13 05:27:17 -0800

    To show the time elapsed on webite

--- src/App.js
+++ src/App.js
@@ -2,7 +2,21 @@ import "./App.css";
 import "./components/Item";
 // import Item from "./components/Item";
 import Car from "./components/Car";
+import { useEffect, useState } from "react";
+let i =0;
 function App() {
+  useEffect(()=>{
+
+    console.log(i++);
+    
+  })
+const [ name, setName]=useState("Amit")
+const updateName=(prams)=>{
+  console.log(prams.target.value)
+  const newName=prams.target.value;
+  setName(newName)
+}
+  console.log("Amit");
   return (
     <div className="App">
       Amit Maurya
@@ -10,6 +24,10 @@ function App() {
       <Item name="Kumar" />
       <Item name="Maurya" /> */}
       <Car></Car>
+      <p>Name : {name}</p>
+      <input onChange={updateName}></input>
+      <input></input>
+      
     </div>
   );
 }
