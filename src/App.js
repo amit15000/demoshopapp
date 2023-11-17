8113ebb3e32062b0d96fc81603184aabdcdc2ba1
Amit Kumar <15000amitkumar@gmail.com>
2023-11-17 06:25:35 -0800

    Start

--- src/App.js
+++ src/App.js
@@ -1,35 +1,7 @@
-import "./App.css";
-import "./components/Item";
-// import Item from "./components/Item";
-import Car from "./components/Car";
-import { useEffect, useState } from "react";
-let i =0;
-function App() {
-  useEffect(()=>{
+import React from "react";
 
-    console.log(i++);
-    
-  })
-const [ name, setName]=useState("Amit")
-const updateName=(prams)=>{
-  console.log(prams.target.value)
-  const newName=prams.target.value;
-  setName(newName)
-}
-  console.log("Amit");
-  return (
-    <div className="App">
-      Amit Maurya
-      {/* <Item name="Geta" />
-      <Item name="Kumar" />
-      <Item name="Maurya" /> */}
-      <Car></Car>
-      <p>Name : {name}</p>
-      <input onChange={updateName}></input>
-      <input></input>
-      
-    </div>
-  );
+function App() {
+  return <div>App</div>;
 }
 
 export default App;
